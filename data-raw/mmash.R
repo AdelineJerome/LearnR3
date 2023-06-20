e0db71075dd2620dfbe4df6578bd6391a1c07b78
Adeline Jerome <adeline.jerome@chalmers.se>
2023-06-20 14:55:02 +0200

    Move summarised dataframes into mmash.R file

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -3,7 +3,9 @@ library(fs)
 library(tidyverse)
 source(here::here("R/functions.R"))
 
-# Download
+
+# Download ----------------------------------------------------------------
+
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
 
 # download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
@@ -31,6 +33,9 @@ mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-
 # )))
 # file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
 
+
+# Import data into df -----------------------------------------------------
+
 user_info_df <- import_multiple_files(
   "user_info.csv",
   import_user_info
@@ -39,3 +44,30 @@ saliva_df <- import_multiple_files(
   "saliva.csv",
   import_saliva
 )
+rr_df <- import_multiple_files(
+  "RR.csv",
+  import_rr
+)
+actigraph_df <- import_multiple_files(
+  "Actigraph.csv",
+  import_actigraph
+)
+
+
+# Summarised data ---------------------------------------------------------
+
+summarised_rr_df <- rr_df %>%
+  group_by(file_path_id, day) %>%
+  summarise(across(ibi_s, list(
+    mean = ~ mean(.x, na.rm = TRUE), # and not mean alone so the treatment of missing value can be precised
+    sd = ~ sd(.x, na.rm = TRUE)
+  ))) %>%
+  ungroup() # to remove some metadata in the code to avoid issues in further wrangling
+
+summarised_actigraph_df <- actigraph_df %>%
+  group_by(file_path_id, day) %>%
+  summarise(across(hr, list(
+    mean = ~ mean(.x, na.rm = TRUE),
+    sd = ~ sd(.x, na.rm = TRUE)
+  ))) %>%
+  ungroup()
