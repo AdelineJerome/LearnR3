633dac91a4fce9e12a15161ba935a8cc37dba142
Adeline Jerome <adeline.jerome@chalmers.se>
2023-06-20 12:31:04 +0200

    importing functions for saliva and user info

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -1,5 +1,7 @@
 library(here) # tell R to first look for the .Rproj file and then start looking for the file we actually want
 library(fs)
+library(tidyverse)
+source(here::here("R/functions.R"))
 
 # Download
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
@@ -7,24 +9,33 @@ mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-
 # download.file(mmash_link, destfile = here("data-raw/mmash-data.zip"))
 # usethis::use_git_ignore("data-raw/mmash-data.zip")
 
-# Unzip
-unzip(here("data-raw/mmash-data.zip"),
-  exdir = here("data-raw"),
-  junkpaths = TRUE
+# # Unzip
+# unzip(here("data-raw/mmash-data.zip"),
+#   exdir = here("data-raw"),
+#   junkpaths = TRUE
+# )
+#
+# unzip(here("data-raw/MMASH.zip"),
+#   exdir = here("data-raw")
+# )
+#
+# # NOTE: You don't need to run this code,
+# # its here to show how we got the file list.
+# #fs::dir_tree("data-raw", recurse = 1)
+#
+# # Remove/tidy up left over files
+# file_delete(here(c(
+#     "data-raw/MMASH.zip",
+#     "data-raw/SHA256SUMS.txt",
+#     "data-raw/LICENSE.txt"
+# )))
+# file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
+
+user_info_df <- import_multiple_files(
+  "user_info.csv",
+  import_user_info
 )
-
-unzip(here("data-raw/MMASH.zip"),
-  exdir = here("data-raw")
+saliva_df <- import_multiple_files(
+  "saliva.csv",
+  import_saliva
 )
-
-# NOTE: You don't need to run this code,
-# its here to show how we got the file list.
-#fs::dir_tree("data-raw", recurse = 1)
-
-# Remove/tidy up left over files
-file_delete(here(c(
-    "data-raw/MMASH.zip",
-    "data-raw/SHA256SUMS.txt",
-    "data-raw/LICENSE.txt"
-)))
-file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
