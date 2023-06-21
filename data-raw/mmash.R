c292a2ddbd96c1835732c463f58fcb9f2371afd9
Adeline Jerome <adeline.jerome@chalmers.se>
2023-06-21 10:25:23 +0200

    created mmash rda data file

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -26,7 +26,10 @@ mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-
 # # NOTE: You don't need to run this code,
 # # its here to show how we got the file list.
 # #fs::dir_tree("data-raw", recurse = 1)
-#
+
+
+# Remove/tidy up left over files ------------------------------------------
+
 # # Remove/tidy up left over files
 # file_delete(here(c(
 #     "data-raw/MMASH.zip",
@@ -59,7 +62,7 @@ actigraph_df <- import_multiple_files(
 # Summarised data ---------------------------------------------------------
 
 summarised_rr_df <- rr_df %>%
-  group_by(file_path_id, day) %>%
+  group_by(user_id, day) %>%
   summarise(across(ibi_s, list(
     mean = ~ mean(.x, na.rm = TRUE), # and not mean alone so the treatment of missing value can be precised
     sd = ~ sd(.x, na.rm = TRUE)
@@ -67,9 +70,29 @@ summarised_rr_df <- rr_df %>%
   ungroup() # to remove some metadata in the code to avoid issues in further wrangling
 
 summarised_actigraph_df <- actigraph_df %>%
-  group_by(file_path_id, day) %>%
+  group_by(user_id, day) %>%
   summarise(across(hr, list(
     mean = ~ mean(.x, na.rm = TRUE),
     sd = ~ sd(.x, na.rm = TRUE)
   ))) %>%
   ungroup()
+
+
+# Combined datasets -------------------------------------------------------
+
+saliva_with_day_df <- saliva_df %>%
+  mutate(day = case_when(
+    samples == "before sleep" ~ 1,
+    samples == "wake up" ~ 2
+  ))
+
+mmash <- list(
+  user_info_df,
+  saliva_with_day_df,
+  summarised_rr_df,
+  summarised_actigraph_df
+) %>%
+  reduce(full_join)
+
+usethis::use_data(mmash, overwrite = TRUE)
+# to create a dataset in data/ (create the folder if it does not exists)
